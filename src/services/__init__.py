3a6f7ff6bd48f60cae5caa2b065acd78b0a89562
Luke Shakespear <shakespearlule@gmail.com>
2026-01-19 21:48:20 +0000

    More fixes to chat bot

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -3,16 +3,22 @@
 from .audio_downloader import AudioDownloader
 from .transcriber import TranscriptionService
 from .pii_detector import PIIDetector
-from .analyzer import AnalysisService
+# Lazy import analyzer to avoid loading qwen_omni_utils on Python 3.9
+# from .analyzer import AnalysisService
 from .voice_fingerprint import VoiceFingerprintService
 from .interactive import InteractiveService, get_interactive_service
 from .interactive_proxy import InteractiveServiceProxy, get_interactive_proxy
+
+def get_analysis_service():
+    """Lazy import for AnalysisService."""
+    from .analyzer import AnalysisService
+    return AnalysisService
 
 __all__ = [
     "AudioDownloader",
     "TranscriptionService", 
     "PIIDetector",
-    "AnalysisService",
+    "get_analysis_service",
     "VoiceFingerprintService",
     "InteractiveService",
     "get_interactive_service",
